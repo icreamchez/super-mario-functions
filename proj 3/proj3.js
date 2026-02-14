8f134bd75305b73979202dffadbcfb706b35baf0
Xinyan <icreamchez@gmail.com>
2026-02-14 11:06:45 -0600

    change color

--- proj 3/proj3.js
+++ proj 3/proj3.js
@@ -1,12 +1,13 @@
 function setup() {
   createCanvas(400, 400);
-    background(100);
+    background(0);
 }
 
 function draw() {
 noStroke(0)
-  fill(200,0,50)
-  ellipse(mouseX, mouseY, 30)
+  fill(mouseX, 100)
+  fill(mouseY, 200)
+  ellipse(mouseX, mouseY, 20)
   
 }
 function mousePressed () {
